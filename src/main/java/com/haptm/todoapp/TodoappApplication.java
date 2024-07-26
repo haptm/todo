622845ad5fcbd86e798c77f1ed68403481313eb8
hapham <phamthanhmanhha03@gmail.com>
2024-07-26 12:06:54 +0700

    add schedule

--- src/main/java/com/haptm/todoapp/TodoappApplication.java
+++ src/main/java/com/haptm/todoapp/TodoappApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @OpenAPIDefinition(
@@ -28,6 +29,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
                 )
         )
 )
+@EnableScheduling
 public class TodoappApplication {
 
     public static void main(String[] args) {
